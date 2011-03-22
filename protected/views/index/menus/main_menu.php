2c6bf12de7a389abbc0077f989604b10acec254b
pklimov <pklimov@quartsoft.com>
2011-03-22 14:59:20 +0000

    Widget "QsMenu" has been updated: property "menuView" has been renamed to "view".

--- protected/views/index/menus/main_menu.php
+++ protected/views/index/menus/main_menu.php
@@ -1,7 +1,7 @@
     <div id="mainmenu">
         <?php //$this->widget('zii.widgets.CMenu',array(
         $this->widget('ext.qs.widgets.QsMenu',array(
-            'menuView' => '//menus/_main_menu',
+            'view' => '//menus/_main_menu',
             'items'=>array(
                 //array('label'=>'Home', 'url'=>array('/site/index')),
                 array('label'=>'Home', 'url'=>array('/')),
