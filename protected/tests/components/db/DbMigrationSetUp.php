0a3ecf14a2fbda5bcc1edcdd2cf831c8c46388b8
pklimov <pklimov@quartsoft.com>
2011-05-19 09:49:20 +0000

    Unit tests have been updated to be more resource efficient.

--- protected/tests/components/db/DbMigrationSetUp.php
+++ protected/tests/components/db/DbMigrationSetUp.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * DbMigrationSetUp class file.
+ *
+ * @author Paul Klimov <pklimov@quart-soft.com> 
+ * @link http://www.quart-soft.com/
+ * @copyright Copyright &copy; 2008-2011 QuartSoft ltd.
+ * @license http://www.quart-soft.com/license/
+ */
 
+/**
+ * This class extends {@link CDbMigration} and was created as helper 
+ * for the unit test creation.
+ */
 class DbMigrationSetUp extends CDbMigration {
     public function up() {
         return true;
