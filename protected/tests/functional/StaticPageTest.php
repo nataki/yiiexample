bcaaa6e401cb4da79d755f08981c9d1ecde7a653
pklimov <pklimov@quartsoft.com>
2011-07-12 09:32:07 +0000

    Functional test "StaticPageTest" has been fixed.

--- protected/tests/functional/StaticPageTest.php
+++ protected/tests/functional/StaticPageTest.php
@@ -8,14 +8,14 @@ class StaticPageTest extends WebTestCase {
     public function testBrowseStaticPages() {
         $staticPages = StaticPage::model()->findAll();
         foreach($staticPages as $staticPage) {
-            $this->open($staticPage->action);
-            $this->assertTextPresent($staticPage->title, 'Could not find title of the static page "'.$staticPage->action.'"!');
+            $this->open($staticPage->url_keyword);
+            $this->assertTextPresent($staticPage->title, 'Could not find title of the static page "'.$staticPage->url_keyword.'"!');
             
             $pageMainContentHtml = $this->getHtmlSource();
             $staticPageNormalizedContent = strip_tags( str_replace( array("\n", "\r"), '', Yii::app()->format->formatEvalView($staticPage->content) ) );
             $pageMainNormalizedContentHtml = strip_tags( str_replace( array("\n", "\r"), '', $pageMainContentHtml ) );
             
-            $this->assertContains($staticPageNormalizedContent, $pageMainNormalizedContentHtml, 'Could not find content of the static page "'.$staticPage->action.'"!');
+            $this->assertContains($staticPageNormalizedContent, $pageMainNormalizedContentHtml, 'Could not find content of the static page "'.$staticPage->url_keyword.'"!');
         }                
     }    
 }
