bc958c5f1b8603fee8f82fa2401f6dc9801bec0d
pklimov <pklimov@quartsoft.com>
2011-11-10 10:36:13 +0000

    Local configuration file example has been updated: mock up http request for the console application has been adjusted.

--- protected/config/__local.php
+++ protected/config/__local.php
@@ -9,24 +9,24 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-if (!isset($_SERVER['HTTP_HOST'])) {
-    // Mockup HTTP params for the console application:
-    $host = 'debug.quart-soft.com';
-    $documentRoot = '/home/www/debug/html';
-    $projectRoot = '/pklimov/yiiexample';
-    $scriptName = $projectRoot.'/index.php';
-    
-    $_SERVER['HTTP_HOST'] = $host;
-    $_SERVER['REQUEST_URI'] = $projectRoot.'/';
-    $_SERVER['SCRIPT_NAME'] = $scriptName;
-    $_SERVER['SCRIPT_FILENAME'] = $documentRoot.$scriptName;
-    $_SERVER['PHP_SELF'] = $scriptName;
-    $_SERVER['DOCUMENT_ROOT'] = $documentRoot;
+if (!array_key_exists('HTTP_HOST', $_SERVER)) {
+    // Mock up HTTP params for the console application:
+    $hostInfo = 'http://debug.quart-soft.com';
+    $baseUrl = '/pklimov/yiiexample';
+    $scriptUrl = $baseUrl.'/index.php';
+    $httpRequestConfig = array(
+        'hostInfo'=>$hostInfo,
+        'baseUrl'=>$baseUrl,
+        'scriptUrl'=>$scriptUrl,
+    );
+} else {
+    $httpRequestConfig = array();
 }
 
 return array(
     // application components
     'components'=>array(        
+        'request'=>$httpRequestConfig,
         'db'=>array(
             'connectionString'=>'mysql:host=localhost;dbname=yiiexample',
             'emulatePrepare'=>true,
@@ -71,9 +71,9 @@ return array(
             'testEmail' => 'pklimov@quart-soft.com',
         ),*/
     ),
-    // uncomment the following to enable the web code generator
-    /*'modules'=>array(
-        'gii'=>array(
+    'modules'=>array(
+        // uncomment the following to enable the web code generator
+        /*'gii'=>array(
             'class'=>'system.gii.GiiModule',
             'ipFilters'=>array(
                 '127.0.0.1',
@@ -83,6 +83,15 @@ return array(
             'generatorPaths'=>array(
                 'ext.qs.gii'
             ),
-        ),            
-    ),*/
+        ),*/
+        // uncomment the following to enable the web PHPUnit test runner
+        /*'phpunit'=>array(
+            'class'=>'ext.qs.modules.phpunit.PhpunitModule',
+            'ipFilters'=>array(
+                '127.0.0.1',
+                '10.10.50.60'
+            ),
+            'password'=>'admin4phpunit',
+        ),*/
+    ),
 );
