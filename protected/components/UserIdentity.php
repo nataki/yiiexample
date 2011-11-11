c9e44b190f65179080e04aca5ce7f00246625066
pklimov <pklimov@quartsoft.com>
2011-11-11 13:07:44 +0000

    Component "UserIdentity" has been updated, allowing log in using both name or email.

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -6,40 +6,33 @@
  * data can identity the user.
  */
 class UserIdentity extends CUserIdentity {
-	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
+	const ERROR_USER_INACTIVE = 3;
+
+    /**
+	 * Authenticates a user, using {@link User} model.
 	 * @return boolean whether authentication succeeds.
 	 */
 	public function authenticate() {
-        $attributes = array(
-            'name' => $this->username,
-            'password' => sha1($this->password),
-            'status_id' => User::STATUS_ACTIVE
-        );
-        $user = User::model()->findByAttributes($attributes);
-        
+        $userFinder = User::model();
+        $userTableAlias = $userFinder->getTableAlias();
+        $user = $userFinder->find("{$userTableAlias}.name=:username OR {$userTableAlias}.email=:username",array(':username'=>$this->username));
+
         if (empty($user)) {
-            $attributes = array(
-                'name' => $this->username                
-            );
-            $user = User::model()->findByAttributes($attributes);
-            if (empty($user)) {
-                $this->errorCode=self::ERROR_USERNAME_INVALID;
-            } else {
-                $this->errorCode=self::ERROR_PASSWORD_INVALID;
-            }
+            $this->errorCode = self::ERROR_USERNAME_INVALID;
         } else {
-            $attributes = $user->getAttributes();
-            foreach($attributes as $attributeName => $attributeValue) {
-                $this->setState($attributeName, $attributeValue);
+            if( strcmp($user->status_id,User::STATUS_ACTIVE)!==0 ) {
+                $this->errorCode = self::ERROR_USER_INACTIVE;
+            } elseif ( strcmp($user->password,sha1($this->password))!==0 ) {
+                $this->errorCode = self::ERROR_PASSWORD_INVALID;
+            } else {
+                $attributes = $user->getAttributes();
+                foreach($attributes as $attributeName => $attributeValue) {
+                    $this->setState($attributeName, $attributeValue);
+                }
+                $this->errorCode = self::ERROR_NONE;
             }
-            $this->errorCode=self::ERROR_NONE;
         }
-        
+
         return !$this->errorCode;
 	}
     
