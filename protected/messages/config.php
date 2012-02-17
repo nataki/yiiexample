92748ad0a307ebb67cf3df0ffda3299106bdf6c5
pklimov <pklimov@quartsoft.com>
2012-02-17 10:31:56 +0000

    Configuration file for the console command "message" has been added.

---
+++ protected/messages/config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This is the configuration for generating message translations
+ * for the application.
+ * It is used by the 'yiic message' command:
+ * <pre>
+ * php -f yiic message messages/config.php
+ * </pre>
+ * @see MessageCommand
+ */
+return array(
+	'sourcePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'messagePath'=>dirname(__FILE__),
+	'languages'=>array('en_us'),
+	'fileTypes'=>array('php'),
+    'overwrite'=>true,
+	'exclude'=>array(
+		'.svn',
+		'/messages',
+		'/tests',
+	),
+);
