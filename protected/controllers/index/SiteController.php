b4dc0018ecd72484bd02505e6a17a9b0f49b77cb
pklimov <pklimov@quartsoft.com>
2012-04-10 11:52:49 +0000

    Some garbage has been removed.

--- protected/controllers/index/SiteController.php
+++ protected/controllers/index/SiteController.php
@@ -61,11 +61,6 @@ class SiteController extends IndexController {
 	 * when an action is not explicitly requested by users.
 	 */
 	public function actionIndex() {
-        /*$dataProvider = User::model()->dataProvider();
-        $users = $dataProvider->getData();
-        CVarDumper::dump($users,10,true);
-        exit;*/
-
         $this->render('index');
 	}
 
