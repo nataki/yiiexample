50eaad3a6fbbdc33baecb8073c2cedee30eb5b47
pklimov <pklimov@quartsoft.com>
2012-07-23 11:16:35 +0000

    Console command "QsConsoleCommandInitApplication" has been updated: user input fetch error has been fixed, action "all" has been composed.

--- protected/install.php
+++ protected/install.php
@@ -8,6 +8,18 @@ $config=array(
     'commandMap'=>array(
         'install'=>array(
             'class'=>'ext.qs.console.commands.QsConsoleCommandInitApplication',
+			'localFilePlaceholderLabels'=>array(
+				'db_host'=>'Database hostname',
+				'db_name'=>'Database name',
+				'db_user'=>'Database access user name',
+				'db_password'=>'Database access password',
+			),
+			'localFilePlaceholderDefaultValues'=>array(
+				'db_host'=>'localhost',
+				'db_name'=>'yiiexample',
+				'db_user'=>'devel',
+				//'db_password'=>'???',
+			),
         ),
     ),
 );
