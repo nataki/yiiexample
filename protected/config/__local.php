ea76759db6934e8420b3797fae6051d83c1feaba
pklimov <pklimov@quartsoft.com>
2012-07-25 12:28:07 +0000

    User dialog in "QsConsoleCommandInitApplication::actionLocalFile" has been adjusted. Local file examples have been updated using placeholders.

--- protected/config/__local.php
+++ protected/config/__local.php
@@ -17,91 +17,91 @@ ini_set('display_errors','on');
 date_default_timezone_set('UTC');
 
 if (!array_key_exists('HTTP_HOST', $_SERVER)) {
-    // Mock up HTTP params for the console application:
-    $hostInfo = '{{hostInfo}}';
-    $baseUrl = '{{baseUrl}}';
-    $scriptUrl = $baseUrl.'/index.php';
-    $httpRequestConfig = array(
-        'hostInfo'=>$hostInfo,
-        'baseUrl'=>$baseUrl,
-        'scriptUrl'=>$scriptUrl,
-    );
+	// Mock up HTTP params for the console application:
+	$hostInfo = '{{hostInfo}}';
+	$baseUrl = '{{baseUrl}}';
+	$scriptUrl = $baseUrl.'/index.php';
+	$httpRequestConfig = array(
+		'hostInfo'=>$hostInfo,
+		'baseUrl'=>$baseUrl,
+		'scriptUrl'=>$scriptUrl,
+	);
 } else {
-    $httpRequestConfig = array();
+	$httpRequestConfig = array();
 }
 
 return array(
-    // application components
-    'components'=>array(        
-        'request'=>$httpRequestConfig,
-        'db'=>array(
-            'connectionString'=>'mysql:host={{dbHost}};dbname={{dbName}}',
-            'emulatePrepare'=>true,
-            'username'=>'{{dbUser}}',
-            'password'=>'{{dbPassword}}',
-            'charset'=>'utf8',
-            'initSQLs' => array(
-                "SET time_zone = '+00:00';"
-            ),
-            //'schemaCachingDuration' => 3600,
-            //'enableParamLogging' => true,
-            //'enableProfiling' => true,            
-        ),
-        'cache'=>array(
-            'class'=>'system.caching.CMemCache',
-            'servers'=>array(
-                array(
-                    'host'=>'localhost', 
-                    'port'=>11211,
-                    'weight'=>100
-                ),
-            ),
-        ),        
-        'log'=>array(            
-            'routes'=>array(                
-                // uncomment the following to show log messages on web pages
-                /*array(
-                    'class'=>'CWebLogRoute',
-                ),*/
-                // uncomment the following to receive log messages via email
-                /*array(
-                    'class'=>'CEmailLogRoute',
-                    'levels'=>'error',
-                    'emails'=>array(
-                        'develqs@quartsoft.com'
-                    ),
-                    'subject'=>'Error at '.exec('hostname'),
-                    'sentFrom'=>'yiiexample@quartsoft.com',
-                ),*/
-            ),                
-        ),
-        // uncomment the following to set up email test mode
-        /*'email'=>array(
-            'testMode' => 'bcc',
-            'testEmail' => 'develqs@quartsoft.com',
-        ),*/
-    ),
-    'modules'=>array(
-        // uncomment the following to enable the web code generator
-        /*'gii'=>array(
-            'class'=>'system.gii.GiiModule',
-            'ipFilters'=>array(
-                '127.0.0.1',
-                '10.10.50.60'
-            ),
-            'password'=>'admin4gii',
-            'generatorPaths'=>array(
-                'ext.qs.gii'
-            ),
-        ),*/
-        // uncomment the following to enable the web PHPUnit test runner
-        /*'phpunit'=>array(
-            'class'=>'ext.qs.test.modules.phpunit.PhpunitModule',
-            'ipFilters'=>array(
-                '127.0.0.1',
-                '10.10.50.60'
-            ),
-            'password'=>'admin4phpunit',
-        ),*/
-    ),
+	// application components
+	'components'=>array(
+		'request'=>$httpRequestConfig,
+		'db'=>array(
+			'connectionString'=>'mysql:host={{dbHost}};dbname={{dbName}}',
+			'emulatePrepare'=>true,
+			'username'=>'{{dbUser}}',
+			'password'=>'{{dbPassword}}',
+			'charset'=>'utf8',
+			'initSQLs' => array(
+				"SET time_zone = '+00:00';"
+			),
+			//'schemaCachingDuration' => 3600,
+			//'enableParamLogging' => true,
+			//'enableProfiling' => true,
+		),
+		'cache'=>array(
+			'class'=>'system.caching.CMemCache',
+			'servers'=>array(
+				array(
+					'host'=>'localhost', 
+					'port'=>11211,
+					'weight'=>100
+				),
+			),
+		),		
+		'log'=>array(
+			'routes'=>array(
+				// uncomment the following to show log messages on web pages
+				/*array(
+					'class'=>'CWebLogRoute',
+				),*/
+				// uncomment the following to receive log messages via email
+				/*array(
+					'class'=>'CEmailLogRoute',
+					'levels'=>'error',
+					'emails'=>array(
+						'develqs@quartsoft.com'
+					),
+					'subject'=>'Error at '.exec('hostname'),
+					'sentFrom'=>'yiiexample@quartsoft.com',
+				),*/
+			),
+		),
+		// uncomment the following to set up email test mode
+		/*'email'=>array(
+			'testMode' => 'bcc',
+			'testEmail' => 'develqs@quartsoft.com',
+		),*/
+	),
+	'modules'=>array(
+		// uncomment the following to enable the web code generator
+		/*'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'ipFilters'=>array(
+				'127.0.0.1',
+				'10.10.50.60'
+			),
+			'password'=>'admin4gii',
+			'generatorPaths'=>array(
+				'ext.qs.gii'
+			),
+		),*/
+		// uncomment the following to enable the web PHPUnit test runner
+		/*'phpunit'=>array(
+			'class'=>'ext.qs.test.modules.phpunit.PhpunitModule',
+			'ipFilters'=>array(
+				'127.0.0.1',
+				'10.10.50.60'
+			),
+			'password'=>'admin4phpunit',
+		),*/
+	),
 );
--- protected/install.php
+++ protected/install.php
@@ -9,7 +9,7 @@
  * php install.php help init
  *
  * In order to run this script in automatic mode use the following:
- * php install.php init all --interactive=0 --config=/path/to/my/config/install.cfg.php
+ * php install.php init all --interactive=0 --config=/path/to/my/config/init.cfg.php
  */
 
 // Change the following paths if necessary:
@@ -23,23 +23,35 @@ $config=array(
 			'class'=>'ext.qs.console.commands.QsConsoleCommandInitApplication',
 			'localFilePlaceholderLabels'=>array(
 				// Database:
-				'dbHost'=>'Database hostname, usually: "localhost"',
-				'dbName'=>'Database name',
+				'dbHost'=>'Database hostname: ip address or domain name, usually: "localhost"',
+				'dbName'=>'Database name (make sure such database exists)',
 				'dbUser'=>'Database access user name',
 				'dbPassword'=>'Database access password',
 				// URL:
-				'hostInfo'=>'URL for the web server root',
+				'hostInfo'=>'URL for the web server root (domain name with leading protocol)',
 				'baseUrl'=>'Part of the URL, which leads for the project web root. At the live server it is usually empty, in development - "/develqs/yiiexample"',
+				// Materials:
+				'materialsBasePath'=>'Base path for the application dynamic files',
+				'materialsBaseUrl'=>'Base URL for the application dynamic files',
+				// PHPUnit:
+				'phpUnitSeleniumIp'=>'Selenium RC server IP address',
+				'phpUnitSeleniumHost'=>'Selenium RC server host name',
 			),
 			'localFilePlaceholderDefaultValues'=>array(
 				// Database:
 				'dbHost'=>'mysql.qs.quart-soft.com',
 				'dbName'=>'yiiexample',
-				'dbUser'=>'devel',
+				//'dbUser'=>'???',
 				//'dbPassword'=>'???',
 				// URL:
 				'hostInfo'=>'http://dev53.quartsoft.com',
 				'baseUrl'=>'',
+				// Materials:
+				'materialsBasePath'=>'/home/www/debug/materials',
+				'materialsBaseUrl'=>'http://materials.quartsoft.com',
+				// PHPUnit:
+				'phpUnitSeleniumIp'=>'10.10.50.60',
+				'phpUnitSeleniumHost'=>'pklimov.qs.quart-soft.com',
 			),
 		),
 	),
