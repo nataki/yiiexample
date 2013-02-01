3b9ead96e1085332528c7ee6e530eacb03286013
Paul Klimov <klimov.paul@gmail.com>
2013-02-01 12:17:03 +0200

    YI-111: Methods formatting date and time from string representation have been removed from "QsFormatter" .

--- protected/extensions/qs/lib/utils/QsFormatter.php
+++ protected/extensions/qs/lib/utils/QsFormatter.php
@@ -11,80 +11,17 @@
 /**
  * Extension of the standard Yii class {@link CFormatter},
  * which extends format methods list with the following features:
- * - format date/time given as string for {@link strtotime}
+ * - evaluate given PHP code
+ * - evaluate given PHP view code
  *
  * @author Paul Klimov <pklimov@quartsoft.com>
  * @package qs.utils
  */
 class QsFormatter extends CFormatter {
-
-	/**
-	 * Checks if the date time value given as string or timestamp.
-	 * If value is timestamp its integer value will be returned.
-	 * If value is a string it will be passed to {@link strtotime} to create a timestamp.
-	 * @param mixed $value the value to be formatted: UNIX timestamp or a string in strtotime format
-	 * @return int the timestamp of value
-	 */
-	protected function normalizeDateTimeValue($value) {
-		if (is_string($value)) {
-			if (ctype_digit($value)) {
-				$value = (int)$value;
-			} else {
-				$value = strtotime($value);
-			}
-		}
-		return $value;
-	}
-
-	/**
-	 * Formats the value as a date.
-	 * @param mixed $value the value to be formatted: UNIX timestamp or a string in strtotime format
-	 * @param mixed $format determines result format, if empty - default will be used
-	 * @return string the formatted result
-	 * @see dateFormat
-	 */
-	public function formatStrDate($value, $format=null) {
-		if (empty($format)) {
-			$format = $this->dateFormat;
-		}
-		$value = $this->normalizeDateTimeValue($value);
-		return date($format, $value);
-	}
-
-	/**
-	 * Formats the value as a time.
-	 * @param mixed $value the value to be formatted: UNIX timestamp or a string in strtotime format
-	 * @param mixed $format determines result format, if empty - default will be used
-	 * @return string the formatted result
-	 * @see timeFormat
-	 */
-	public function formatStrTime($value, $format=null) {
-		if (empty($format)) {
-			$format = $this->timeFormat;
-		}
-		$value = $this->normalizeDateTimeValue($value);
-		return date($format, $value);
-	}
-
-	/**
-	 * Formats the value as a date and time.
-	 * @param mixed $value the value to be formatted: UNIX timestamp or a string in strtotime format
-	 * @param mixed $format determines result format, if empty - default will be used
-	 * @return string the formatted result
-	 * @see datetimeFormat
-	 */
-	public function formatStrDateTime($value, $format=null) {
-		if (empty($format)) {
-			$format = $this->datetimeFormat;
-		}
-		$value = $this->normalizeDateTimeValue($value);
-		return date($format, $value);
-	}
-
 	/**
 	 * Formats the value evaluating it as PHP expression.
 	 * @param mixed $value the value to be formatted: a PHP expression or PHP callback to be evaluated.
-	 * @param array $_data_ additional parameters to be passed to the above expression/callback.
+	 * @param array $data additional parameters to be passed to the above expression/callback.
 	 * @return string the formatted result
 	 */
 	public function formatEval($value, $data=array()) {
@@ -94,7 +31,7 @@ class QsFormatter extends CFormatter {
 	/**
 	 * Formats the value evaluating it as view file content.
 	 * @param mixed $value the value to be formatted: view code (HTNL with embeded PHP).
-	 * @param array $_data_ additional parameters to be passed to the above expression/callback.
+	 * @param array $data additional parameters to be passed to the above expression/callback.
 	 * @return string the formatted result
 	 */
 	public function formatEvalView($value, $data=null) {
@@ -110,7 +47,7 @@ class QsFormatter extends CFormatter {
 	protected function evalRender($_viewStr_, array $_data_=null) {
 		$_evalStr_ = '?>'.$_viewStr_;
 		if (is_array($_data_)) {
-			extract($_data_,EXTR_PREFIX_SAME,'data');
+			extract($_data_, EXTR_PREFIX_SAME, 'data');
 		}
 		ob_start();
 		ob_implicit_flush(false);
--- protected/extensions/qs/tests/utils/QsFormatterTest.php
+++ protected/extensions/qs/tests/utils/QsFormatterTest.php
@@ -5,63 +5,8 @@
  * @see QsFormatter
  */
 class QsFormatterTest extends CTestCase {
-
 	public static function setUpBeforeClass() {
 		Yii::import('ext.qs.lib.utils.QsFormatter');
-	}
-
-	public function testFormatStrDate() {
-		$formatter = new QsFormatter();
-
-		$dateString = 'NOW +'.rand(0, 30).' days';
-		$dateTimestamp = strtotime($dateString);
-
-		$expectedFormattedDate = date($formatter->dateFormat, $dateTimestamp);
-		$formattedDate = $formatter->formatStrDate($dateString);
-		$this->assertEquals( $formattedDate, $expectedFormattedDate, 'Unable to format date from string!' );
-		$formattedDate = $formatter->formatStrDate($dateTimestamp);
-		$this->assertEquals( $formattedDate, $expectedFormattedDate, 'Unable to format date from timestamp!' );
-
-		$customFormat = 'Y|m|d';
-		$expectedFormattedDate = date($customFormat, $dateTimestamp);
-		$formattedDate = $formatter->formatStrDate($dateString, $customFormat);
-		$this->assertEquals( $formattedDate, $expectedFormattedDate, 'Unable to format date from string with custom format!' );
-	}
-
-	public function testFormatStrTime() {
-		$formatter = new QsFormatter();
-
-		$dateString = 'NOW +'.rand(0, 30).' days';
-		$dateTimestamp = strtotime($dateString);
-
-		$expectedFormattedTime = date($formatter->timeFormat, $dateTimestamp);
-		$formattedTime = $formatter->formatStrTime($dateString);
-		$this->assertEquals( $formattedTime, $expectedFormattedTime, 'Unable to format time from string!' );
-		$formattedTime = $formatter->formatStrTime($dateTimestamp);
-		$this->assertEquals( $formattedTime, $expectedFormattedTime, 'Unable to format time from timestamp!' );
-
-		$customFormat = 'H|i|s';
-		$expectedFormattedTime = date($customFormat, $dateTimestamp);
-		$formattedTime = $formatter->formatStrTime($dateString, $customFormat);
-		$this->assertEquals( $formattedTime, $expectedFormattedTime, 'Unable to format time from string with custom format!' );
-	}
-
-	public function testFormatStrDateTime() {
-		$formatter = new QsFormatter();
-
-		$dateString = 'NOW +'.rand(0, 30).' days';
-		$dateTimestamp = strtotime($dateString);
-
-		$expectedFormattedDateTime = date($formatter->datetimeFormat, $dateTimestamp);
-		$formattedDateTime = $formatter->formatStrDateTime($dateString);
-		$this->assertEquals( $formattedDateTime, $expectedFormattedDateTime, 'Unable to format datetime from string!' );
-		$formattedDateTime = $formatter->formatStrDateTime($dateTimestamp);
-		$this->assertEquals( $formattedDateTime, $expectedFormattedDateTime, 'Unable to format datetime from timestamp!' );
-
-		$customFormat = 'Y|m|d H|i|s';
-		$expectedFormattedDateTime = date($customFormat, $dateTimestamp);
-		$formattedDateTime = $formatter->formatStrDateTime($dateString, $customFormat);
-		$this->assertEquals( $formattedDateTime, $expectedFormattedDateTime, 'Unable to format datetime from string with custom format!' );
 	}
 
 	public function testFormatEval() {
